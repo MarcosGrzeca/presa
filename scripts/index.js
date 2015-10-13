519497dc9379957a9595c38243166bbd464ce343
MarcosGrzeca <marcos-a-g@hotmail.com>
2015-10-13 20:27:12 -0300

    Opção para adicionar agente

--- scripts/index.js
+++ scripts/index.js
@@ -56,3 +56,38 @@ function teste() {
 		indice++;
 	}
 }
+
+function abrirPopupAdicionarAgente() {
+	$('#modalAdicionarAgente').modal("show").on('shown.bs.modal', function (e) {
+  		var html = "";
+  		for (i = 1; i <= $("#nroLinhas").val(); i++) {
+  			html += "<option value='" + (i -1) + "'>" + i + "</option>";
+  		}
+  		$("#novaLinha").html(html);
+  		$("#novaLinha").val(0);
+
+  		var html = "";
+  		for (i = 1; i <= $("#nroColunas").val(); i++) {
+  			html += "<option value='" + (i -1) + "'>" + i + "</option>";
+  		}
+  		$("#novaColuna").html(html);
+  		$("#novaColuna").val(0);
+	})
+}
+
+function adicionarAgente()  {
+	var elemento = Ambiente.getPosicao($("#novaLinha").val(), $("#novaColuna").val());
+	if (elemento == 0) {
+		console.log($("#novaLinha").val() + ' -- ' + $("#novaColuna").val());
+		if ($("#novoAgente").val() == "presa") {
+			algoritmo.populacao.createPresa(parseInt($("#novaLinha").val()), parseInt($("#novaColuna").val()));
+		} else {
+			algoritmo.populacao.createPredador(parseInt($("#novaLinha").val()), parseInt($("#novaColuna").val()));
+		}
+		Ambiente.atualizar();
+		Ambiente.clonarMapa();
+		$('#modalAdicionarAgente').modal("hide");
+	} else {
+		alert('A posição indicada já possui um animal');
+	}
+}
