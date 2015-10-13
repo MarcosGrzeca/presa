da58aac1dcd66b2b7d7afa335c200d9e8ff2a31f
MarcosGrzeca <marcos-a-g@hotmail.com>
2015-10-12 21:30:48 -0300

    mais uma versao

--- scripts/populacao.js
+++ scripts/populacao.js
@@ -9,7 +9,7 @@ function Populacao(nroPredadores, nroPresas) {
 	}
 	
 	this.gerarPopulacao = function() {
-		for (var i = 0; i < nroPresas; i++) {
+		/*for (var i = 0; i < nroPresas; i++) {
 			var presa = new Presa(this.contAnimais);
 			presa.gerarPosicaoAleatoria();
 			this.animais.push(presa);
@@ -20,16 +20,16 @@ function Populacao(nroPredadores, nroPresas) {
 			predador.gerarPosicaoAleatoria();
 			this.animais.push(predador);
 			this.contAnimais++;
-		}
-//		this.gerarPopulacaoTeste();
+		}*/	
+		this.gerarPopulacaoTeste();
 	};
 
 	this.gerarPopulacaoTeste = function() {
 		this.createPresa(2, 1);
 		this.createPredador(1, 1);
 		this.createPredador(1, 2);
-		this.createPredador(3, 2);
-		this.createPredador(0, 0);
+		//this.createPredador(3, 2);
+		//this.createPredador(0, 0);
 		//this.createPredador(1, 3);
 		//this.createPredador(3, 3);
 	};
@@ -42,7 +42,10 @@ function Populacao(nroPredadores, nroPresas) {
 		while (animaisNaFila.length > 0) {
 			var animais = $.extend(true, [], animaisNaFila);
 			var animaisNaFila = [];
-			/*$.each(animais, function(key, animal) {
+			$.each(animais, function(key, animal) {
+				if (animal instanceof Presa) {
+					temPresa = true;
+				}
 				status = animal.move(indice);
 				if (status == 1) {
 					animaisNaFila.push(animal);
@@ -52,9 +55,9 @@ function Populacao(nroPredadores, nroPresas) {
 					//animal.incrementarIteracao();
 					animal.setPassosRealizados(0);
 				}
-			});*/
+			});
 			
-			while (animais.length > 0) {
+			/*while (animais.length > 0) {
 				var random = gerarRandomico(animais.length, 0);
 				var animal = animais[random];
 				if (animal instanceof Presa) {
@@ -71,7 +74,7 @@ function Populacao(nroPredadores, nroPresas) {
 				} 
 				animais.splice(random, 1);
 			}
-		
+		*/
 			$.each(mortos, function(key, animal) {
 				animal.morre();
 			});
@@ -83,6 +86,7 @@ function Populacao(nroPredadores, nroPresas) {
 			algoritmo.pararSimulacao();
 			alert("Todas as presas foram capturadas");
 		}
+		//debugger;
 	};
 
 	this.createPredador = function(linha, coluna) {
